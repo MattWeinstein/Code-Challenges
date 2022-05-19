653a71374ce0fc0e6a29a201db71d6c0231dba18
Matt Weinstein <mattw412@gmail.com>
2022-05-18 23:06:44 -0400

    Update Perfect-Squares-Perfect-Fun.js

--- Code Wars/Numbers/Perfect-Squares-Perfect-Fun.js
+++ Code Wars/Numbers/Perfect-Squares-Perfect-Fun.js
@@ -13,11 +13,30 @@ Note: 4 digits so 2 squared (2x2 perfect square). 2 digits on each line.
 Note: 9 digits so 3 squared (3x3 perfect square). 3 digits on each line.*/
 
 function squareIt(int) {
-  let n = Math.sqrt(int.toString().length);
-  
-  if(n !== Math.floor(n)){
+  //Getting the sq root of the parameter using the global math object
+  let sqRoot = Math.sqrt(int.toString().length)
+  //Setting global variables we will use and manipulate later
+  let answer = ''
+  let nums
+  //Condiitonal to check if we have a perfect square 
+  if(sqRoot%1===0){
+    //If modulus/1 leaves no remainder, it is
+    for(let i = 1;i<=sqRoot;i++){
+      //Loop to create the block to return to the console
+      if(i===1){
+        //Will only need the new line charachter 1 less than number of rows
+        nums=int.toString().slice(0,sqRoot)
+        //Slice the original parameter from 0 to the square root (inclusive) on first iteration
+        answer=nums
+      }else{
+        //Slice the next part of the string at i*sq root, and i-1
+        nums = int.toString().slice((i-1)*sqRoot,(i*sqRoot))
+        answer+=`\n${nums}`
+      }
+    }
+  return answer
+  } else{
+    //If modulus/1 leaves a remainder, it is not a perfect square
     return 'Not a perfect square!'
-  } else {
-    return int.toString().split('').map((e,i) => (i+1) % n === 0 && i !== (n**2) - 1 ? `${e}\n` : e).join('');
   }
 }
