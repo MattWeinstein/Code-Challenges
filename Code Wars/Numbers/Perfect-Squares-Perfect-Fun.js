c01006267779dd08705efcc726609606cd85d34d
Matt Weinstein <mattw412@gmail.com>
2022-05-18 23:03:39 -0400

    Create Perfect-Squares-Perfect-Fun.js

---
+++ Code Wars/Numbers/Perfect-Squares-Perfect-Fun.js
@@ -0,0 +1,28 @@
+/*Given an integer, if the length of it's digits is a perfect square, return a square block of sqroot(length) * sqroot(length). If not, simply return "Not a perfect square!".
+
+Examples:
+
+1212 returns:
+
+12
+12 
+
+Note: 4 digits so 2 squared (2x2 perfect square). 2 digits on each line.
+
+123123123 returns:
+
+123
+123
+123
+
+Note: 9 digits so 3 squared (3x3 perfect square). 3 digits on each line.*/
+
+function squareIt(int) {
+  let n = Math.sqrt(int.toString().length);
+  
+  if(n !== Math.floor(n)){
+    return 'Not a perfect square!'
+  } else {
+    return int.toString().split('').map((e,i) => (i+1) % n === 0 && i !== (n**2) - 1 ? `${e}\n` : e).join('');
+  }
+}
